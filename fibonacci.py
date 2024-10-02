20dcdbbeb32e5aa1f0b376a69808636476214c26
jonathan-tan-tan <jonathan.mdev23@gmail.com>
2024-10-02 09:20:40 -0500

    adding new solution

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,23 @@
+""" 
+    Serie Fibonacci
+escribe un programa que es riba los 50 primeros numeros de la serie de Fibonacci empezando en 0.
+la serie de Fibonacci se compone poruna sucesión de números en la que el siguiente es la suma de los dos anteriores.
+0,1,1,2,3,5,8,13,21
+
+"""
+
+def fibonacci(n):
+    serie=[0,1]
+    if n<2:
+        print(serie[n-1])
+    else:
+        for i in range (2,n):
+            nuevo=serie[-1]+serie[-2]
+            serie.append(nuevo)
+    print(serie)        
+
+"""
+
 def fibonacci(n):
     fib_1=0
     fib_2=1
@@ -10,7 +30,9 @@ def fibonacci(n):
             print(fib_sig, end=" ")
             fib_1=fib_2
             fib_2=fib_sig
+            
+
+"""            
 
 fibonacci(50)
-
 
